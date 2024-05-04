fec79fb5833a4d306fffcbc81e35ede78a5011a0
ralph-hatoum <hatoumralph@gmail.com>
2024-05-04 11:11:31 +0200

    sign and verify endpoints

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 import encrypt_decrypt
+import sign_verify
 
 app = FastAPI()
 
@@ -12,3 +13,18 @@ async def encrypt(request: Request):
 async def decrypt(request: Request):
     request_json = await request.json()
     return encrypt_decrypt.decrypt_dict(request_json)
+
+@app.post("/sign")
+async def sign(request: Request):
+    request_json = await request.json()
+    signature = sign_verify.sign(request_json)
+    return {"signature":signature}
+
+@app.post("/verify")
+async def verify(request: Request):
+    request_json = await request.json()
+    valid = sign_verify.verify(request_json)
+    if not valid:
+        raise HTTPException(status_code=400, detail="Signature invalid")
+    else:
+        return None
