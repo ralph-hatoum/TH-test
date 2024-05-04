8c4c273dc598724bc803dbadceb7956567130f32
ralph-hatoum <hatoumralph@gmail.com>
2024-05-04 11:11:20 +0200

    verify function

--- sign_verify.py
+++ sign_verify.py
@@ -6,9 +6,13 @@ import json
 key = b"secret_key"
 
 def sign(payload):
-    print(payload)
     payload = encrypt_decrypt.decrypt_dict(payload)
-    print(payload)
     payload = json.dumps(payload, separators=(',', ':')).encode()
     signature = hmac.new(key, payload, hashlib.sha256).hexdigest()
     return signature
+
+def verify(payload):
+    payload_signature = payload["signature"]
+    payload_data = payload["data"]
+    signature = sign(payload_data)
+    return payload_signature == signature
