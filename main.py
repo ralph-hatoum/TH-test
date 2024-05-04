536fbf5abeececf4b1f05b423919843a0c69c217
ralph-hatoum <hatoumralph@gmail.com>
2024-05-04 12:23:36 +0200

    refactored variable names

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ app = FastAPI()
 
 @app.post("/encrypt")
 async def encrypt(request: Request):
-    request_json = await request.json()
-    return encrypt_decrypt.encrypt_dict(request_json)
+    payload = await request.json()
+    return encrypt_decrypt.encrypt_dict(payload)
 
 @app.post("/decrypt")
 async def decrypt(request: Request):
-    request_json = await request.json()
-    return encrypt_decrypt.decrypt_dict(request_json)
+    payload = await request.json()
+    return encrypt_decrypt.decrypt_dict(payload)
 
 @app.post("/sign")
 async def sign(request: Request):
-    request_json = await request.json()
-    signature = sign_verify.sign(request_json)
+    payload = await request.json()
+    signature = sign_verify.sign(payload)
     return {"signature":signature}
 
 @app.post("/verify")
 async def verify(request: Request):
-    request_json = await request.json()
-    valid = sign_verify.verify(request_json)
+    payload = await request.json()
+    valid = sign_verify.verify(payload)
     if not valid:
         raise HTTPException(status_code=400, detail="Signature invalid")
     else:
